253a60380b27ea670f606819a1c734197a52aa45
Gmarik561 <101624796+Gmarik561@users.noreply.github.com>
2024-05-15 08:48:44 +0300

    Update app.py

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from werkzeug.urls import url_quote
 from werkzeug.urls import url_quote_plus as url_quote
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,4 +9,4 @@ def hello():
     return 'Hello, welcome to my Flask app!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='http://flaskapp', port=5000)
